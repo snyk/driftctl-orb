0b673797bce2fb96cfb0284ae566851a4efbaa99
William Beuil <william.beuil@gmail.com>
2021-12-15 15:10:52 +0100

    Bump dctlenv

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,3 +1,7 @@
+VersionLowerOrEqual() {
+    [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n 1)" ]
+}
+
 GetVersion() {
     if [ -n "${PARAM_VERSION}" ]; then
       echo -n "${PARAM_VERSION}"
@@ -8,13 +12,21 @@ GetVersion() {
 
 Install() {
     export DCTL_NO_VERSION_CHECK="true"
+
     BINPATH="${HOME}/.dctlenv/bin"
-    gpg --import driftctl_pubkey.pem
     if [ ! -d "${HOME}/.dctlenv" ]; then
-      git clone --depth 1 --branch v0.1.3 https://github.com/wbeuil/dctlenv ~/.dctlenv
+      git clone --depth 1 --branch v0.1.7 https://github.com/wbeuil/dctlenv ~/.dctlenv
     fi
+
     VERSION=$(GetVersion)
-    DCTLENV_PGP=1 "${BINPATH}/dctlenv" use "${VERSION}"
+
+    if VersionLowerOrEqual "${VERSION/v/}" "0.9.1"; then
+      "${BINPATH}/dctlenv" use "${VERSION}"
+    else
+      gpg --import driftctl_pubkey.pem
+      DCTLENV_PGP=1 "${BINPATH}/dctlenv" use "${VERSION}"
+    fi
+
     if [ ! -e /usr/local/bin/driftctl ]; then
       sudo ln -s "${BINPATH}/driftctl" /usr/local/bin/driftctl
     fi
